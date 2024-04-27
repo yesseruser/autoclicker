681925a34a85da4f6a5ce32bb9601ec164577908
yesseruser <yesseruseryt@gmail.com>
2024-04-27 19:42:49 +0200

    Move LeftMouseClick method to Clicker class

--- AutoClickerLib/Clicker.cs
+++ AutoClickerLib/Clicker.cs
@@ -7,11 +7,18 @@ public class Clicker
         while (true)
         {
             Win32Apis.GetCursorPos(out var pos);
-            Win32Apis.LeftMouseClick(pos.X, pos.Y);
+            LeftMouseClick(pos.X, pos.Y);
             
             await Task.Delay(100, cancellationToken);
             if (cancellationToken.IsCancellationRequested)
                 break;
         }
+    }
+
+    public static void LeftMouseClick(int xpos, int ypos)
+    {
+        Win32Apis.SetCursorPos(xpos, ypos);
+        Win32Apis.mouse_event(Win32Apis.MouseEventLeftDown, xpos, ypos, 0, 0);
+        Win32Apis.mouse_event(Win32Apis.MouseEventLeftUp, xpos, ypos, 0, 0);
     }
 }
--- AutoClickerLib/Win32Apis.cs
+++ AutoClickerLib/Win32Apis.cs
@@ -5,7 +5,7 @@ namespace AutoClickerLib;
 public class Win32Apis
 {
     [DllImport("user32.dll")]
-    static extern bool SetCursorPos(int x, int y);
+    public static extern bool SetCursorPos(int x, int y);
     
     [DllImport("user32.dll")]
     public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
@@ -15,11 +15,4 @@ public class Win32Apis
     
     public const int MouseEventLeftDown = 0x02;
     public const int MouseEventLeftUp = 0x04;
-    
-    public static void LeftMouseClick(int xpos, int ypos)
-    {
-        SetCursorPos(xpos, ypos);
-        mouse_event(MouseEventLeftDown, xpos, ypos, 0, 0);
-        mouse_event(MouseEventLeftUp, xpos, ypos, 0, 0);
-    }
 }
